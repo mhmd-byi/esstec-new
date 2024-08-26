14017359c6ac40e77e0a39921ff6bdff5cbe941b
mhmd-byi <mohammed@buildyourinnovation.com>
2024-08-26 17:11:47 +0530

    replacing forward slash with html code

--- src/app/common/components/menu.js
+++ src/app/common/components/menu.js
@@ -2,29 +2,29 @@ export const Menu = () => {
   return (
     <div className="uppercase">
       <p className="flex flex-col">
-        <span className="font-medium">// About</span>
-        <span>philosophy /</span>
-        <span>clients /</span>
-        <span>team /</span>
+        <span className="font-medium">&#47;&#47; About</span>
+        <span>philosophy &#47;</span>
+        <span>clients &#47;</span>
+        <span>team &#47;</span>
       </p>
       <p className="flex flex-col">
-        <span className="font-medium">// creative expertise</span>
-        <span>digital /</span>
-        <span>physical /</span>
+        <span className="font-medium">&#47;&#47; creative expertise</span>
+        <span>digital &#47;</span>
+        <span>physical &#47;</span>
       </p>
       <p className="flex flex-col">
-        <span className="font-medium">// project showcase</span>
-        <span>ewaa abu dhabi /</span>
-        <span>raw coffee company /</span>
-        <span>scope investments /</span>
-        <span>arabian knights /</span>
-        <span>market i research /</span>
-        <span>lvmh fragrances /</span>
+        <span className="font-medium">&#47;&#47; project showcase</span>
+        <span>ewaa abu dhabi &#47;</span>
+        <span>raw coffee company &#47;</span>
+        <span>scope investments &#47;</span>
+        <span>arabian knights &#47;</span>
+        <span>market i research &#47;</span>
+        <span>lvmh fragrances &#47;</span>
       </p>
       <p className="flex flex-col">
-        <span className="font-medium">// contact</span>
-        <span>email /</span>
-        <span>phone /</span>
+        <span className="font-medium">&#47;&#47; contact</span>
+        <span>email &#47;</span>
+        <span>phone &#47;</span>
       </p>
     </div>
   );
