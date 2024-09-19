c6dc0f6e566138007d13f8a90e0b4b692612eba7
mhmd-byi <mohammed@buildyourinnovation.com>
2024-09-19 18:27:55 +0530

    adding philosophy component

--- src/app/common/components/philosophy.jsx
+++ src/app/common/components/philosophy.jsx
@@ -1,9 +1,19 @@
 export const PhilosophyComponent = () => {
-    return (
-        <div className="absolute top-56 mt-10 w-[1100px] items-center justify-center">
-            <h2 className="text-6xl font-bold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px]">
-                philosophy
-            </h2>
-        </div>
-    )
-}
+  return (
+    <div className="absolute top-56 mt-10 w-[1100px] items-center justify-center">
+      <h2 className="text-6xl font-bold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px]">
+        philosophy
+      </h2>
+      <div className="mt-52 text-center max-w-[970px]">
+        <p className="text-text-primary uppercase text-base mb-6">
+          we are the creatives who...
+        </p>
+        <p className="text-text-primary uppercase text-base mb-6">
+          believe in meaningful work and authentic connections over bagging
+          contracts.
+        </p>
+        <p className="text-text-primary uppercase text-base">*</p>
+      </div>
+    </div>
+  );
+};
