0e1269c1624970c4aba634db2f0c170df0f0497f
mhmd-byi <mohammed@buildyourinnovation.com>
2024-09-19 15:43:00 +0530

    changing ewaa slider

--- src/app/common/components/projects/ewaa.jsx
+++ src/app/common/components/projects/ewaa.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Carousel } from "@material-tailwind/react";
+import { Carousel } from "flowbite-react";
 import slide1 from "@/assets/projectImages/ewaa/1-brand-guidelines.svg";
 import slide2 from "@/assets/projectImages/ewaa/2-brand-guidelines.svg";
 import slide3 from "@/assets/projectImages/ewaa/3-animation-video.svg";
@@ -11,16 +11,7 @@ import slide7 from "@/assets/projectImages/ewaa/7-corporate-film.svg";
 export const EwaaCarouselComponent = () => {
   return (
     <div className="absolute top-44 left-44 mt-2 w-[64vw] h-[68.7vh] items-center justify-center">
-      <Carousel className="rounded-[27.5px] z-50" loop={true} autoplay={true} autoplayDelay={5000} transition={{type: "spring", duration: 0.5}} navigation={({ length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-          {new Array(length).fill("").map((_, i) => (
-            <span
-              key={i}
-              className="hidden"
-            />
-          ))}
-        </div>
-      )}>
+      <Carousel className="rounded-[27.5px] z-50" slideInterval={5000} indicators={false}>
         <Image
             alt="brand guide"
             src={slide1}
