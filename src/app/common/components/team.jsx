7548741e3213c31116210a11565383ecd7406f9d
mhmd-byi <mohammed@buildyourinnovation.com>
2024-09-21 14:33:46 +0530

    adjusting font size for team component

--- src/app/common/components/team.jsx
+++ src/app/common/components/team.jsx
@@ -4,18 +4,18 @@ export const TeamComponent = () => {
             <h2 className="text-6xl font-bold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px]">
                 Team
             </h2>
-            <p className="text-4xl font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
+            <p className="text-base font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
                 We are an electric team of multicultural individuals uniunited by a shared love for all things design.
                 each of us brings unique expertise to every idea and conversation.
             </p>
-            <p className="text-4xl font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
+            <p className="text-base font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
                 We let our projects dictate the required skills, ensuring progress is never hindered and always backed by our can-do attitude.
             </p>
-            <p className="text-4xl font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
+            <p className="text-base font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
                 collaborating across the world with humans and pulling in animals (yes, essential for R&D), we maintain connecticonnections
                 across asia, the middle east, africa and europe.
             </p>
-            <p className="text-4xl font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
+            <p className="text-base font-semibold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px] mt-10">
                 whether you are interested in collaborating, working on a project together, or just stopping by for a chat over coffee,
                 we're here. reach out to us at info@esstec.ae
             </p>
