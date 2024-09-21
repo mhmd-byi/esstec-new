0c054223e6870c227a9b6eee28c08cb2ad8ae78a
mhmd-byi <mohammed@buildyourinnovation.com>
2024-09-21 10:42:23 +0530

    adding anchor tag to links

--- src/app/common/components/client.jsx
+++ src/app/common/components/client.jsx
@@ -2,7 +2,7 @@ import { clientData } from "./clientsData"
 
 export const ClientComponent = () => {
     return (
-        <div className="absolute top-56 mt-10 w-[1100px] items-center justify-center">
+        <div className="absolute top-56 mt-10 w-[1100px] items-center justify-center z-50">
             <h2 className="text-6xl font-bold text-text-primary uppercase duration-1000 ease-in-out text-center max-w-[970px]">
                 clients
             </h2>
@@ -10,7 +10,7 @@ export const ClientComponent = () => {
                 {clientData.map((data, index) => (
                     <div className="basis-1/4 justify-between py-5" key={index}>
                         <div className="text-text-primary font-medium text-sm uppercase">{data.name}</div>
-                        <div className="text-text-primary font-medium text-sm lowercase">{data.website}</div>
+                        <div className="text-text-primary font-medium text-sm lowercase"><a href={`https://${data.website}`} target="_blank" className="cursor-pointer">{data.website}</a></div>
                     </div>
                 ))}
             
