412637d671568e979d4be56d900bb4ddb63d264c
mhmd-byi <mohammed@buildyourinnovation.com>
2024-10-15 12:47:12 +0530

    aligning box to left and right

--- src/app/common/components/draw.js
+++ src/app/common/components/draw.js
@@ -1,36 +1,41 @@
-import React, { useRef, useEffect } from 'react';
-import { ClientComponent } from './client';
-import { PhilosophyComponent } from './philosophy';
+import React, { useRef, useEffect } from "react";
+import { ClientComponent } from "./client";
+import { PhilosophyComponent } from "./philosophy";
 import { EwaaCarouselComponent } from "./projects/ewaa";
-import { ScopeInvestmentsCarouselComponent } from './projects/scope-investments';
-import { MarketIResearchCarouselComponent } from './projects/market-i-research';
-import { EmailComponent } from './email';
-import { PhoneComponent } from './phone';
-import { RawCoffeeCarouselComponent } from './projects/raw-coffee';
-import { FreshlyMealsCarouselComponent } from './projects/freshly-meals';
-import { LVMHCarouselComponent } from './projects/lvmh';
-import { TeamComponent } from './team';
-import { DDYCarouselComponent } from './projects/ddy';
-import { ArabianKnightsCarouselComponent } from './projects/arabian-knights';
-import { ExpertiseComponent } from './expertise';
-import { checkForScreenSizeInDraw } from '@/helper/helper';
+import { ScopeInvestmentsCarouselComponent } from "./projects/scope-investments";
+import { MarketIResearchCarouselComponent } from "./projects/market-i-research";
+import { EmailComponent } from "./email";
+import { PhoneComponent } from "./phone";
+import { RawCoffeeCarouselComponent } from "./projects/raw-coffee";
+import { FreshlyMealsCarouselComponent } from "./projects/freshly-meals";
+import { LVMHCarouselComponent } from "./projects/lvmh";
+import { TeamComponent } from "./team";
+import { DDYCarouselComponent } from "./projects/ddy";
+import { ArabianKnightsCarouselComponent } from "./projects/arabian-knights";
+import { ExpertiseComponent } from "./expertise";
+import { checkForScreenSizeInDraw } from "@/helper/helper";
 
-function DrawingComponent({ draw, activeMenu, animationComplete, setAnimationComplete }) {
+function DrawingComponent({
+  draw,
+  activeMenu,
+  animationComplete,
+  setAnimationComplete,
+}) {
   const rectRef = useRef(null);
 
   useEffect(() => {
     if (draw && rectRef.current) {
       // Calculate the total perimeter of the rectangle
-      const height = parseInt(rectRef.current.getAttribute('height'));
-      const width = parseInt(rectRef.current.getAttribute('width'));
+      const height = parseInt(rectRef.current.getAttribute("height"));
+      const width = parseInt(rectRef.current.getAttribute("width"));
       const perimeter = 2 * (width + height);
       rectRef.current.style.strokeDasharray = perimeter;
       rectRef.current.style.strokeDashoffset = perimeter;
 
       requestAnimationFrame(() => {
-        rectRef.current.style.transition = 'stroke-dashoffset 5s ease-out, fill 0.5s ease-in-out 4.5s';
-        rectRef.current.style.strokeDashoffset = '0';
-        rectRef.current.style.fill = '#F3BB44';
+        rectRef.current.style.transition = "stroke-dashoffset 5s ease-out, fill 0.5s ease-in-out 4.5s";
+        rectRef.current.style.strokeDashoffset = "0";
+        rectRef.current.style.fill = "#F3BB44";
 
         setTimeout(() => {
           setAnimationComplete(true); // Set the state to true after the animation
@@ -40,72 +45,73 @@ function DrawingComponent({ draw, activeMenu, animationComplete, setAnimationCom
   }, [draw]);
 
   const checkForScreenSize = checkForScreenSizeInDraw();
+  const hasWindow = typeof window !== "undefined";
+  const widthOfScreen = hasWindow ? window.innerWidth : null;
+  const heightOfScreen = hasWindow ? window.innerHeight : null;
 
   return (
     <>
-      {checkForScreenSize && <div className='absolute inset-0 flex justify-center items-center'>
-        <svg viewBox={checkForScreenSize.viewBox}>
-          {draw && (
-            <rect
-              ref={rectRef}
-              x={checkForScreenSize.xAxis}
-              y={checkForScreenSize.yAxis}
-              width={checkForScreenSize.rectangleWidth} // Width of the rectangle
-              height={checkForScreenSize.rectangleHeight} // Height of the rectangle
-              rx="25" // Rounded corner radius
-              ry="25" // Rounded corner radius
-              fill="transparent"
-              stroke="#222222"
-              strokeWidth={checkForScreenSize.strokeWidth}
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              transform="scale(1 , -1) translate(0, -550.5)"
-            />
-          )}
-        </svg>
-      </div>}
-      {(draw && animationComplete && activeMenu === 'philosophy') && (<div className='px-20'>
-        <PhilosophyComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'clients') && (<div className='px-20'>
-        <ClientComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'ewaa') && (<div className='px-20'>
-        <EwaaCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'scope') && (<div className='px-20'>
-        <ScopeInvestmentsCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'market') && (<div className='px-20'>
-        <MarketIResearchCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'email') && (<div className='px-20'>
-        <EmailComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'phone') && (<div className='px-20'>
-        <PhoneComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'coffee') && (<div className='px-20'>
-        <RawCoffeeCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'freshly') && (<div className='px-20'>
-        <FreshlyMealsCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'lvmh') && (<div className='px-20'>
-        <LVMHCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'team') && (<div className='px-20'>
-        <TeamComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'ddy') && (<div className='px-20'>
-        <DDYCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'arabian') && (<div className='px-20'>
-        <ArabianKnightsCarouselComponent />
-      </div>)}
-      {(draw && animationComplete && activeMenu === 'expertise') && (<div className='px-20'>
-        <ExpertiseComponent />
-      </div>)}
+      {checkForScreenSize && (
+        <div className={`absolute inset-0 flex justify-center items-center px-10 lg:px-40`}>
+          <svg viewBox="0 0 1100 600">
+            {draw && (
+              <rect
+                ref={rectRef}
+                x={5}
+                y={5}
+                width={widthOfScreen - 670} // Width of the rectangle
+                height={heightOfScreen - 500} // Height of the rectangle
+                rx="35" // Rounded corner radius
+                ry="35" // Rounded corner radius
+                fill="transparent"
+                stroke="#222222"
+                strokeWidth={12}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transform="scale(1 , -1) translate(0, -550.5)"
+              />
+            )}
+          </svg>
+        </div>
+      )}
+      {draw && animationComplete && (
+        <div className="px-20">
+          {(() => {
+            switch (activeMenu) {
+              case "philosophy":
+                return <PhilosophyComponent />;
+              case "clients":
+                return <ClientComponent />;
+              case "ewaa":
+                return <EwaaCarouselComponent />;
+              case "scope":
+                return <ScopeInvestmentsCarouselComponent />;
+              case "market":
+                return <MarketIResearchCarouselComponent />;
+              case "email":
+                return <EmailComponent />;
+              case "phone":
+                return <PhoneComponent />;
+              case "coffee":
+                return <RawCoffeeCarouselComponent />;
+              case "freshly":
+                return <FreshlyMealsCarouselComponent />;
+              case "lvmh":
+                return <LVMHCarouselComponent />;
+              case "team":
+                return <TeamComponent />;
+              case "ddy":
+                return <DDYCarouselComponent />;
+              case "arabian":
+                return <ArabianKnightsCarouselComponent />;
+              case "expertise":
+                return <ExpertiseComponent />;
+              default:
+                return null; // default case to handle any unanticipated activeMenu values
+            }
+          })()}
+        </div>
+      )}
     </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -11,62 +11,93 @@ import { MobileHeader } from "./common/components/mobileHeader";
 import MobileDrawingComponent from "./common/components/mobileDraw";
 
 export default function Home() {
-  
   const [draw, setDraw] = useState(false);
   const [animationComplete, setAnimationComplete] = useState(false);
-  const [activeMenu, setActiveMenu] = useState('');
+  const [activeMenu, setActiveMenu] = useState("");
   const handleDrawClick = () => {
     if (!draw) {
       setDraw(true);
     }
   };
-  
+
   return (
     <main className="flex min-h-screen flex-col px-10 lg:px-40 pt-5 lg:pt-16 bg-bg-primary">
-      <div className="hidden lg:flex flex-row justify-between text-xs leading-6">
-        <Header />
+      <div className="hidden md:block">
+        <div className="flex flex-row justify-between text-xs leading-6">
+          <Header />
+        </div>
+        <div className="text-right mt-8 lg:mt-16 z-50">
+          <Menu
+            handleDrawClick={handleDrawClick}
+            setActiveMenu={setActiveMenu}
+            setDraw={setDraw}
+            setAnimationComplete={setAnimationComplete}
+            activeMenu={activeMenu}
+          />
+        </div>
+        <div>
+          <DrawingComponent
+            draw={draw}
+            activeMenu={activeMenu}
+            animationComplete={animationComplete}
+            setAnimationComplete={setAnimationComplete}
+          />
+        </div>
+        <div className="mt-44 lg:-mt-10">
+          <p class="block md:hidden text-text-primary text-sm mt-20 mb-60 text-center">
+            For best experience please visit the website on your laptop, we are
+            soon coming with our mobile design
+          </p>
+          <Image src={esstecLogo} width="w-full" height="h-full" alt="logo" />
+        </div>
+        <div className="hidden lg:block mt-10 mb-5">
+          <Footer />
+        </div>
       </div>
-      <div className="hidden md:block text-right mt-8 lg:mt-16 z-50">
-        <Menu 
-          handleDrawClick={handleDrawClick} 
-          setActiveMenu={setActiveMenu} 
-          setDraw={setDraw}
-          setAnimationComplete={setAnimationComplete}
-          activeMenu={activeMenu}
-        />
-      </div>
-      <div className="hidden lg:block">
-        <DrawingComponent 
-          draw={draw} 
-          activeMenu={activeMenu} 
-          animationComplete={animationComplete} 
-          setAnimationComplete={setAnimationComplete} 
-        />
-      </div>
-      <div className="hidden my-5">
-        <hr />
-      </div>
-      <div className="hidden">
-        <MobileHeader />
-      </div>
-      <div className="hidden">
-        <MobileDrawingComponent 
-          draw={draw} 
-          activeMenu={activeMenu} 
-          animationComplete={animationComplete} 
-          setAnimationComplete={setAnimationComplete} 
-        />
-      </div>
-      <div className="mt-44 lg:-mt-10">
-        <p class="block md:hidden text-text-primary text-sm mt-20 mb-60 text-center">For best experience please visit the website on your laptop, we are soon coming with our mobile design</p>
-        <Image src={esstecLogo} width="w-full" height="h-full" alt="logo" />
-      </div>
-      <div className="hidden lg:block mt-10 mb-5">
-        <Footer />
-      </div>
+      {/* showing components in desktop */}
+      {/* Desktop section ends here */}
+
+
+
+
+
+      {/* Mobile section starts here */}
       <div className="block md:hidden">
-        <MobileFooter />
+        <div className="text-right mt-8 lg:mt-16 z-50">
+          <Menu
+            handleDrawClick={handleDrawClick}
+            setActiveMenu={setActiveMenu}
+            setDraw={setDraw}
+            setAnimationComplete={setAnimationComplete}
+            activeMenu={activeMenu}
+          />
+        </div>
+        <div className="my-5">
+          <hr />
+        </div>
+        <div>
+          <MobileHeader />
+        </div>
+        <div>
+          <MobileDrawingComponent
+            draw={draw}
+            activeMenu={activeMenu}
+            animationComplete={animationComplete}
+            setAnimationComplete={setAnimationComplete}
+          />
+        </div>
+        <div className="mt-44 lg:-mt-10">
+          <p class="text-text-primary text-sm mt-20 mb-60 text-center">
+            For best experience please visit the website on your laptop, we are
+            soon coming with our mobile design
+          </p>
+          <Image src={esstecLogo} width="w-full" height="h-full" alt="logo" />
+        </div>
+        <div>
+          <MobileFooter />
+        </div>
       </div>
+      {/* showing components in mobile */}
     </main>
   );
 }
