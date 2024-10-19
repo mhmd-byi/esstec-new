4d3bf0e91f985c62aeef9901da7c3a364925c2e8
mhmd-fl <mohammed@founderandlightning.com>
2024-10-20 01:35:42 +0530

    changing border placement

--- src/helper/mobileContainer.jsx
+++ src/helper/mobileContainer.jsx
@@ -1,6 +1,6 @@
 export const MobileContainer = ({ children }) => {
   return (
-    <div className="w-full min-h-72 max-h-72 border-4 border-text-primary rounded-2xl flex justify-center">
+    <div className="border-4 border-text-primary w-full min-h-72 max-h-72 rounded-2xl flex justify-center">
       {children}
     </div>
   );
