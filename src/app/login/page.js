c04899cf2d269e9377c5243c1bda39054ff3a7fa
mhmd-byi <mohammed@buildyourinnovation.com>
2024-10-19 18:27:16 +0530

    setting border color for login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -37,7 +37,7 @@ export default function Login() {
     <section>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <Image src={esstecLogo} className="w-3/4 md:w-1/4" alt="logo" />
-        <div className="w-full bg-white rounded-lg shadow border mt-14 sm:max-w-md xl:p-0">
+        <div className="w-full bg-white rounded-lg shadow border border-text-primary mt-14 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-text-primary md:text-2xl">
               Sign in
