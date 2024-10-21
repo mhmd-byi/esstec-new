4b3d08ee9078c446d195f520c78e59ef126502cc
mhmd-fl <mohammed@founderandlightning.com>
2024-10-21 16:52:27 +0530

    adding dynamic titles to freshly meals

--- src/app/common/components/projects/freshly-meals.jsx
+++ src/app/common/components/projects/freshly-meals.jsx
@@ -1,21 +1,29 @@
-"use client";
-import Image from "next/image";
-import slide1 from "@/assets/projectImages/freshly-meals/1-user-flow-design.svg";
-import slide2 from "@/assets/projectImages/freshly-meals/2-interface-design.svg";
-import slide3 from "@/assets/projectImages/freshly-meals/3-app-development.svg";
-import slide4 from "@/assets/projectImages/freshly-meals/4-app-development.svg";
-import slide5 from "@/assets/projectImages/freshly-meals/5-app-package-code.svg";
-import slide6 from "@/assets/projectImages/freshly-meals/6-backend-admin-panel.svg";
-import slide7 from "@/assets/projectImages/freshly-meals/7-in-app-emailers.svg";
-import { ChevronLeftIcon } from "../../icons/ChevronLeftIcon";
-import { ChevronRightIcon } from "../../icons/ChevronRightIcon";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+'use client';
+import Image from 'next/image';
+import slide1 from '@/assets/projectImages/freshly-meals/1-user-flow-design.svg';
+import slide2 from '@/assets/projectImages/freshly-meals/2-interface-design.svg';
+import slide3 from '@/assets/projectImages/freshly-meals/3-app-development.svg';
+import slide4 from '@/assets/projectImages/freshly-meals/4-app-development.svg';
+import slide5 from '@/assets/projectImages/freshly-meals/5-app-package-code.svg';
+import slide6 from '@/assets/projectImages/freshly-meals/6-backend-admin-panel.svg';
+import slide7 from '@/assets/projectImages/freshly-meals/7-in-app-emailers.svg';
+import { ChevronLeftIcon } from '../../icons/ChevronLeftIcon';
+import { ChevronRightIcon } from '../../icons/ChevronRightIcon';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const FreshlyMealsCarouselComponent = () => {
-  const slides = [slide1, slide2, slide3, slide4, slide5, slide6, slide7];
-  const hasWindow = typeof window !== "undefined";
+  const slides = [
+    { slide: slide1, title: 'user flow design' },
+    { slide: slide2, title: 'interface design' },
+    { slide: slide3, title: 'app development' },
+    { slide: slide4, title: 'app development' },
+    { slide: slide5, title: 'app package code' },
+    { slide: slide6, title: 'backend admin panel' },
+    { slide: slide7, title: 'in-app emailers' },
+  ];
+  const hasWindow = typeof window !== 'undefined';
   const widthOfScreen = hasWindow ? window.innerWidth : null;
   const NextArrow = (props) => {
     const { className, style, onClick } = props;
@@ -24,9 +32,9 @@ export const FreshlyMealsCarouselComponent = () => {
         className={className}
         style={{
           ...style,
-          display: "block",
-          right: widthOfScreen < 500 ? "5px" : "30px",
-          color: "#222222",
+          display: 'block',
+          right: widthOfScreen < 500 ? '5px' : '30px',
+          color: '#222222',
           zIndex: 10,
         }}
         onClick={onClick}
@@ -43,9 +51,9 @@ export const FreshlyMealsCarouselComponent = () => {
         className={className}
         style={{
           ...style,
-          display: "block",
-          left: "10px",
-          color: "#222222",
+          display: 'block',
+          left: '10px',
+          color: '#222222',
           zIndex: 10,
         }}
         onClick={onClick}
@@ -69,13 +77,19 @@ export const FreshlyMealsCarouselComponent = () => {
   return (
     <div className="h-full w-full">
       <Slider {...settings} className="h-full w-full">
-        {slides.map((src, index) => (
-          <div key={index} className="h-full w-full">
+        {slides.map((slide, index) => (
+          <div
+            key={index}
+            className="relative h-full w-full pointer-events-none"
+          >
             <Image
-              src={src}
+              src={slide.slide}
               alt={`Slide ${index + 1}`}
-              className="h-[17.5rem] rounded-xl md:h-full w-full object-cover"
+              className="h-[17.5rem] rounded-xl md:h-full w-full object-cover pointer-events-none"
             />
+            <div className="absolute border-2 border-text-primary -bottom-px md:bottom-20 -ml-1.5 md:ml-0 md:right-0 w-[102%] md:w-[24.5rem] md:text-3xl bg-text-primary text-center text-bg-primary font-bold uppercase py-2 md:py-6 z-10 rounded-b-xl md:rounded-none pointer-events-none">
+              {slide.title}
+            </div>
           </div>
         ))}
       </Slider>
