2572b8bbebffdfc85b42892a562e90b2faa02f70
mhmd-fl <mohammed@founderandlightning.com>
2024-10-21 15:53:56 +0530

    making clients single column in mobile and adjusting position of dynamic title div

--- src/app/common/components/client.jsx
+++ src/app/common/components/client.jsx
@@ -6,7 +6,7 @@ export const ClientComponent = () => {
       <h2 className="text-center text-3xl font-bold uppercase text-text-primary duration-1000 ease-in-out sm:text-4xl md:text-5xl lg:text-6xl md:mt-20 md:-mb-20">
         clients
       </h2>
-      <div className="grid gap-4 text-left text-xs uppercase text-text-primary grid-cols-2 md:grid-cols-3 md:text-sm lg:grid-cols-4 md:pl-12 lg:pl-24">
+      <div className="grid gap-4 text-center md:text-left text-xs uppercase text-text-primary grid-cols-1 md:grid-cols-3 md:text-sm lg:grid-cols-4 md:pl-12 lg:pl-24">
         {clientData.map((client, index) => (
           <div key={index} className="">
             <p>{client.name}</p>
