97cc2d38436d9d6714a0574079a7d3997f41ba2e
mhmd-fl <mohammed@founderandlightning.com>
2024-10-26 22:23:20 +0530

    changing env variable

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -44,7 +44,7 @@ export const getOperatingSystem = () => {
 export const getRandomQuotes = async () => {
   const category = 'imagination';
   const url = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
-  const apiKey = process.env.NEXT_PUBLIC_QUOTES_API_KEY;
+  const apiKey = process.env.NEXT_PUBLIC_QUOTES_API;
 
   try {
     const response = await fetch(url, {
