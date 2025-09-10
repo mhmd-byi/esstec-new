06746e06483562b928fc709878574586c1076f87
mhmd-byi <142663836+mhmd-byi@users.noreply.github.com>
2025-09-10 13:49:30 +0530

    Update client.ts

--- db/client.ts
+++ db/client.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || 'mongodb+srv://mhmd:Mhmd%4066426633@kal-graphql.8ubsnp3.mongodb.net/esstec?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URI;
 if (!uri) {
   throw new Error('Please define the MONGODB_URI environment variable');
 }
